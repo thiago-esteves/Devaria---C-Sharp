6b2a240d2eef8b85d51707f54445d7cf874a99b2
thiago-esteves <testeves.mao@gmail.com>
2021-05-04 17:57:24 -0400

    aula estruturaçaoDeRepeticao

---
+++ estruturaDeRepeticao/estruturaDeRepeticao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace estruturaDeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] argumentos)
+        {
+            if (argumentos.Length == 0)
+            {
+                Console.WriteLine("Favor informar palavras no argumento");
+                return;
+            }
+
+            //for 
+            for (var indice = 0; indice < argumentos.Length; indice++)
+            {
+                Console.WriteLine($"Estrutura for, indice e valor:{indice},{argumentos[indice]}");
+            }
+            // while
+            var argumentosLidos = 0;
+            while (argumentosLidos < argumentos.Length)
+            {
+                Console.WriteLine($"Estrutura while,argumentosLidos e valor lido:{argumentosLidos},{argumentos[argumentosLidos]}");
+                argumentosLidos++;
+            }
+            // do while
+            var loopEfetuados = 0;
+            do
+            {
+                console.WriteLine($"Estrutura do while, loop infinito:{}")
+            } while (true);
+
+        }
+
+    }
+}
