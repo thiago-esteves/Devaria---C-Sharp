7038ebe6f4e3c49e8e3a1dd878832f431a150d90
thiago-esteves <81878335+thiago-esteves@users.noreply.github.com>
2021-06-03 15:40:43 -0300

    MeuPrimeiroApi

---
+++ MinhaPrimeiraApi/MinhaPrimeiraApi/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraApi.Controllers
+
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult ObterUsuario()
+        {
+            return Ok(" ola mundo, meu primeiro API Csharp");
+        }
+    }
+}
